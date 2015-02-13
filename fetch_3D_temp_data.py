7c6958db2d7ec4ed7029f7279308d7863577bac1
rdecal <decal@uw.edu>
2015-02-13 14:53:15 -0800

    turned all sections into functions that can be called from the second script

--- fetch_3D_temp_data.py
+++ fetch_3D_temp_data.py
@@ -20,56 +20,48 @@ Created on Fri Feb 13 11:19:31 2015
 import scipy.io as io
 import os.path
 
-#==============================================================================
-#  File I/O
-#==============================================================================
+def fileio():
+    mydir = os.path.dirname(__file__)
+    print "Current directory is: \n", mydir, "\n \n"
+    return mydir
 
-mydir = os.path.dirname(__file__)
-print """Current directory is: \n""", mydir, "\n \n"
+def load_temp_data(mydir):
+    print "Loading temperature data"
+    print "--------------------------- \n"
+    print """Enter temperature data file name. If none entered, default is %s """ % "lhstore2.mat  \n"
+    Temp_data_file = raw_input("Input temperature data file dir: ")
+    if Temp_data_file == '':
+        print "No input, using lhstore2.mat \n"
+        Temp_data_file = os.path.join(mydir, "data", 'lhstore2.mat')
+    Temp_data = io.loadmat(Temp_data_file)
+    #pull out temp data 
+    T_raw = Temp_data['store2'].T
+    return T_raw
 
-#==============================================================================
-# Loading temp data
-#==============================================================================
-print "Loading temperature data"
-print "--------------------------- \n"
-print """Enter temperature data file name. If none entered, default is %s """ % "lhstore2.mat  \n"
-Temp_data_file = raw_input("Input temperature data file dir: ")
-if Temp_data_file == '':
-    print "No input, using lhstore2.mat \n"
-    Temp_data_file = os.path.join(mydir, "data", 'lhstore2.mat')
-Temp_data = io.loadmat(Temp_data_file)
-
-
-#pull out temp data 
-T_raw = Temp_data['store2'].T
+def load_elevation_data(mydir):
+    print "Loading elevation data" , " \n --------------------------- \n"
+    print """Enter eleation data file name. If none entered, default is %s """ % "z-positions.mat  \n"
+    z_data_file = raw_input("Input elevation data file dir: ")
+    if z_data_file == '':
+        print "No input, using z-positions.mat \n"
+        z_data_file = os.path.join(mydir, "data", 'z-positions.mat')
+    z = io.loadmat(z_data_file)
     
-#==============================================================================
-# Loading z file
-#==============================================================================
-print "Loading elevation data"
-print "--------------------------- \n"
-print """Enter eleation data file name. If none entered, default is %s """ % "z-positions.mat  \n"
-z_data_file = raw_input("Input elevation data file dir: ")
-if z_data_file == '':
-    print "No input, using z-positions.mat \n"
-    z_data_file = os.path.join(mydir, "data", 'z-positions.mat')
-z = io.loadmat(z_data_file)
+    #then pull out elevation data (z)
+    zpos = z['y'][0]
+    return zpos
 
-#then pull out elevation data (z)
-zpos = z['y'][0]        
+def load_positional_data(mydir):
+    print "Loading positional data"
+    print "--------------------------- \n"
+    print """Enter positional data file name. If none entered, default is %s """ % "final-lh50.mat  \n"
+    pos_data_file = raw_input("Input positional data file dir: ")
+    if pos_data_file == '':
+        print "No input, using final-lh50.mat \n"
+        pos_data_file = os.path.join(mydir, 'data', 'final-lh50.mat')
+    pos_data = io.loadmat(pos_data_file) #x,y positional data
+    return pos_data
 
-
-#==============================================================================
-# Loading positional data
-#==============================================================================
-print "Loading positional data"
-print "--------------------------- \n"
-print """Enter positional data file name. If none entered, default is %s """ % "final-lh50.mat  \n"
-pos_data_file = raw_input("Input positional data file dir: ")
-if pos_data_file == '':
-    print "No input, using final-lh50.mat \n"
-    pos_data_file = os.path.join(mydir, 'data', 'final-lh50.mat')
-pos_data = io.loadmat(pos_data_file) #x,y positional data
 
 #==============================================================================
 # lh50_data.keys() ==> ['p_in', 'p_mm', 'p', 's', 'store', '__header__', '__globals__',  '__version__']
@@ -81,3 +73,12 @@ pos_data = io.loadmat(pos_data_file) #x,y positional data
 # 'z' => elevation positions
 #==============================================================================
 
+def main():
+    mydir = fileio()
+    T_raw = load_temp_data(mydir)
+    zpos = load_elevation_data(mydir)
+    pos_data = load_positional_data(mydir)
+    return mydir, T_raw, zpos, pos_data
+    
+if __name__ == "__main__":
+    main()
