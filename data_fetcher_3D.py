7f9d880e130f00b7c78ddff47e5add460daf520d
rdecal <decal@uw.edu>
2015-02-13 17:58:06 -0800

    improved docs, removed unneeded fxn

--- data_fetcher_3D.py
+++ data_fetcher_3D.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-fetch_3D_data
+This script opens data files and extracts relevant data. 
 
 Input:
-User input filename string
+User input filename strings
  
 Output:
-temperature data and the positional data as variables (without conditional names, as with the LH-oriented name scheme in the file now). 
-Maybe this output can be a class? I'm not sure if that's useful here. Anyway, that output should get entered into the second script. 
-
+temperature data
+positional data
+elevation data
 
 TODO:
 (maybe) save data to txt files so we don't have to re-run this code every time?
@@ -19,11 +19,6 @@ Created on Fri Feb 13 11:19:31 2015
 
 import scipy.io as io
 import os.path
-
-def fileio():
-    mydir = os.path.dirname(__file__)
-    print "Current directory is: \n", mydir, "\n \n"
-    return mydir
 
 def load_temp_data(mydir):
     print "Loading temperature data"
@@ -74,7 +69,8 @@ def load_positional_data(mydir):
 #==============================================================================
 
 def main():
-    mydir = fileio()
+    mydir = os.path.dirname(__file__)
+    print "Current directory is: \n", mydir, "\n \n"
     T_raw = load_temp_data(mydir)
     zpos = load_elevation_data(mydir)
     pos_data = load_positional_data(mydir)
