d0fda860ba5b54a1e10c67574d9cf5123045ffe9
rdecal <decal@uw.edu>
2015-02-13 18:36:55 -0800

    trying to turn this into a modular script

--- process_3D_data.py
+++ process_3D_data.py
@@ -15,39 +15,48 @@ mydir, T_raw, zpos, pos_data = data_fetcher_3D.main()
 #TODO: average these repeated points, instead of deleting them with lim
 lim = 199      #limit of points used (to remove repeat positions or unwanted positions)
 
-#Make array of observed locations (x,y)
-xy_observed = np.zeros((2,lim),dtype = float)
-observed_data = np.zeros((3,lim), dtype = float)
+def main():
+    """doing this so that we can call this file as a module. Would be way more ideal if this entire page
+    was broken up into unit functions and made Pythonic"""
+    #Make array of observed locations (x,y)
+    xy_observed = np.zeros((2,lim),dtype = float)
+    observed_data = np.zeros((3,lim), dtype = float)
+    
+    #temperatures_time_avg = pos_data['s']      #time averaged temperature data
+    xy_observed[0,:] = pos_data['p_mm'][:lim,0]          #x (crosswind) axis, observed data
+    xy_observed[1,:] = pos_data['p_mm'][:lim,1] #Is this the y axis? -Richard
+    
+    #for 3d interpolation:
+    xyz_observed = np.zeros((3,len(xy_observed.T)),dtype = float)       #observed locations
+    xyz_observed[:2,:] = xy_observed
+    xyz_observed[2,:] = zpos[3]*np.ones((1,lim))
+    T_time_avg_3d = np.mean(T_raw[:lim,0,:],1)      #preallocate observed measurements variable
+    T_sd = np.std(T_raw[:lim,0,:],1)    #preallocate sd variable
+    
+    for i in range(1,4):
+        T_slice = np.mean(T_raw[:lim,i,:], 1)      #single height layer of temperature   
+        T_slice_sd = np.std(T_raw[:lim,i,:],1)
+        layer = len(xy_observed.T)      
+        xy_slice = np.append(xy_observed,(np.ones((1,layer))*zpos[3-i]),axis = 0)     #single height layer of position data
+        xyz_observed = np.append(xyz_observed, xy_slice, axis = 1)      #3d positions
+        T_time_avg_3d = np.append(T_time_avg_3d, T_slice, axis = 1)       #resized tempearture data
+        T_sd = np.append(T_sd,T_slice_sd,axis = 1)
+    
+    #look for and remove nans ()
+    #TODO (maybe): turn this into a function
+    if np.any(np.isnan(T_time_avg_3d)):
+        NaN_locations = np.where(np.isnan(T_time_avg_3d))      #find the indices of Nans
+        T_time_avg_3d = np.delete(T_time_avg_3d, NaN_locations)
+        xyz_observed = np.delete(xyz_observed, NaN_locations, axis = 1)
+        T_sd = np.delete(T_sd, NaN_locations)
+    
+    #fit all of the observed data into one array, for ease of use of fitting function
+    observed_data_3d = np.zeros((4,len(xyz_observed.T)), dtype = float)
+    observed_data_3d[:3,:] = xyz_observed
+    observed_data_3d[3,:] = T_time_avg_3d.T
+    
+    return T_sd
 
-#temperatures_time_avg = pos_data['s']      #time averaged temperature data
-xy_observed[0,:] = pos_data['p_mm'][:lim,0]          #x (crosswind) axis, observed data
-xy_observed[1,:] = pos_data['p_mm'][:lim,1] #Is this the y axis? -Richard
 
-#for 3d interpolation:
-xyz_observed = np.zeros((3,len(xy_observed.T)),dtype = float)       #observed locations
-xyz_observed[:2,:] = xy_observed
-xyz_observed[2,:] = zpos[3]*np.ones((1,lim))
-T_time_avg_3d = np.mean(T_raw[:lim,0,:],1)      #preallocate observed measurements variable
-T_sd = np.std(T_raw[:lim,0,:],1)    #preallocate sd variable
-
-for i in range(1,4):
-    T_slice = np.mean(T_raw[:lim,i,:], 1)      #single height layer of temperature   
-    T_slice_sd = np.std(T_raw[:lim,i,:],1)
-    layer = len(xy_observed.T)      
-    xy_slice = np.append(xy_observed,(np.ones((1,layer))*zpos[3-i]),axis = 0)     #single height layer of position data
-    xyz_observed = np.append(xyz_observed, xy_slice, axis = 1)      #3d positions
-    T_time_avg_3d = np.append(T_time_avg_3d, T_slice, axis = 1)       #resized tempearture data
-    T_sd = np.append(T_sd,T_slice_sd,axis = 1)
-
-#look for and remove nans ()
-#TODO (maybe): turn this into a function
-if np.any(np.isnan(T_time_avg_3d)):
-    NaN_locations = np.where(np.isnan(T_time_avg_3d))      #find the indices of Nans
-    T_time_avg_3d = np.delete(T_time_avg_3d, NaN_locations)
-    xyz_observed = np.delete(xyz_observed, NaN_locations, axis = 1)
-    T_sd = np.delete(T_sd, NaN_locations)
-
-#fit all of the observed data into one array, for ease of use of fitting function
-observed_data_3d = np.zeros((4,len(xyz_observed.T)), dtype = float)
-observed_data_3d[:3,:] = xyz_observed
-observed_data_3d[3,:] = T_time_avg_3d.T
+if __name__ == "__main__":
+    main()
