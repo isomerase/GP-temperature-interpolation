1f9899f2a7f48f84f33cfb48ac369c84e8d5aea1
rdecal <decal@uw.edu>
2015-02-15 11:51:17 -0800

    broke up main() into separate functions

--- GaussianProc_predictor_3D.py
+++ GaussianProc_predictor_3D.py
@@ -22,30 +22,38 @@ import matplotlib.pyplot as plt
 
 import process_3D_data
 
-def main():
-    #load vars from second script
-    T_sd, T_time_avg_3d, xyz_observed = process_3D_data.main() 
-
-    #prediction locations, make ##finish your sentences, points deducted -Richard
-    #TODO: what are these magic numbers?
+def predictionLocations():
+    """prediction locations, make ##finish your sentences, points deducted -Richard
+    
+    TODO: document what these magic numbers mean
+    Do we want to feed any input into this fxn?"""
+    
     x_predict = np.atleast_2d(np.linspace(0, 254, 25))       #2 mm prediction sites
     y_predict = np.atleast_2d(np.linspace(100, 850, 15))
     z_predict = np.atleast_2d(np.linspace(80, 280, 20))
     
     x1,x2,x3 = np.meshgrid(x_predict, y_predict, z_predict)
     xyz_predict = np.vstack([x1.reshape(x1.size), x2.reshape(x2.size), x3.reshape(x3.size)]).T #TODO: what is this line for?
-    
-    #calculate noise (required)
-    nugget =  (T_sd/T_time_avg_3d)**2
+    return xyz_predict
+
+def noiseCalc(temp_stdev, Temp_time_avg_3D):
+    nugget =  (temp_stdev/Temp_time_avg_3D)**2
     nugget = nugget       #deletes repeated measurment locations
-      
-    #TODO: make section into separate function 
+    return nugget
+
+def gaussianProcess(T_sd, T_time_avg_3d, xyz_observed):
+    """
+    TODO: feed params for gp (corr, thetas, etc) into the parent fxn?
+    """
+    xyz_predict = predictionLocations()
+    mynugget = noiseCalc(T_sd, T_time_avg_3d)    
+    
     gp = gaussian_process.GaussianProcess(corr = 'absolute_exponential',
                                           theta0 = 1./25, 
                                           thetaL = 1e-1,
                                           thetaU = .3,
                                           normalize = True,
-                                          nugget = nugget)
+                                          nugget = mynugget)
     #when height = 1, thetaL = .1, thetaU = .3
     #when height = 0, thetaL = 10e-2,thetaU = .3
     
@@ -58,8 +66,14 @@ def main():
     
     T_prediction, y_prediction_MSE = gp.predict(xyz_predict, eval_MSE = True)   #produce predicted y values
     sigma = np.sqrt(y_prediction_MSE)   #get SD of fit at each x_predicted location (for confidence interval)
+    return T_prediction, y_prediction_MSE, sigma
+
+def main():
+    #load vars from second script
+    T_sd, T_time_avg_3d, xyz_observed = process_3D_data.main()
     
-    
+    #run Gaussian process
+    T_prediction, y_prediction_MSE, sigma = gaussianProcess(T_sd, T_time_avg_3d, xyz_observed)
     return T_prediction, y_prediction_MSE, sigma
 
 
